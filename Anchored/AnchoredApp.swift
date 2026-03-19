c81c7155f1fb8dadaaf282ca4c12f240ca9cf940
Garrett Crane <garrett.crane@yahoo.com>
2026-03-18 21:49:57 -0700

    Initial Commit

---
+++ Anchored/AnchoredApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnchoredApp.swift
+//  Anchored
+//
+//  Created by Garrett Crane on 3/18/26.
+//
+
+import SwiftUI
+
+@main
+struct AnchoredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
